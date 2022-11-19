a0afe89b9eacffe3a1d2b4e825372d7690b57b1e
unknown <reinanalves1988@gmail.com>
2022-11-19 10:36:36 -0300

    send Notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,14 +1,31 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './style.css';
 
-function NotificationButton() {
-    return (
-        <>
-            <div className="dsmeta-red-btn">
-                <img src={icon} alt="Notificar" />
-            </div>
-        </>
-    );
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log(`Id: ${id} capturado com sucesso!`)
+        })
+}
+
+function NotificationButton({ saleId }: Props) {
+
+
+return (
+    <>
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" onClick={()=> handleClick(saleId)}/>
+        </div>
+    </>
+);
 }
 
 export default NotificationButton;
+
+
