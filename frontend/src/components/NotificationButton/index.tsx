7bc1a3efdf42085cd8e121a8afca16c4251c7549
unknown <reinanalves1988@gmail.com>
2022-11-19 15:10:43 -0300

    5

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,30 @@
+import { toast } from 'react-toastify';
+import axios from "axios";
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from "../../utils/requests";
+
+import './styles.css';
+
+
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            toast.info("SMS enviado com sucesso!");
+        });
+}
+
+function NotificationButton({saleId} : Props) {
+    return (
+        <div className="dsmeta-red-btn" onClick={()=> handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+
+}
+
+export default NotificationButton;
