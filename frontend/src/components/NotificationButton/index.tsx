3689d902965e41556d2e5d3e913a0baf524484fa
unknown <reinanalves1988@gmail.com>
2022-11-19 16:19:43 -0300

    8

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 import axios from "axios";
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from "../../utils/request";
-import './styles.css';
+import './style.css';
 
 type Props = {
     saleId: number;
