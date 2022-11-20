6aebca64478160d4d2d263399cc96d0df6477b0d
unknown <reinanalves1988@gmail.com>
2022-11-19 23:07:40 -0300

    10

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -56,7 +56,7 @@ function SalesCard() {
                 <table className="dsmeta-sales-table">
                     <thead>
                         <tr>
-                            <th className="show992">Id</th>
+                            <th className="show992">ID</th>
                             <th className="show576">Data</th>
                             <th>Vendedor</th>
                             <th className="show992">Visitas</th>
